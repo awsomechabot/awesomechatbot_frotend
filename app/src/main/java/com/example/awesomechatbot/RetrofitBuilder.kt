dac35eb1cc5bdac04b722ed56d22a624b73c6baf
pbkdpwls <pbkdpwls@naver.com>
2021-08-02 17:27:25 +0900

    login

--- app/src/main/java/com/example/awesomechatbot/RetrofitBuilder.kt
+++ app/src/main/java/com/example/awesomechatbot/RetrofitBuilder.kt
@@ -8,7 +8,7 @@ object RetrofitBuilder {
 
     init {
         val retrofit = Retrofit.Builder()
-            .baseUrl("http://172.30.1.52:3000")
+            .baseUrl("http://192.168.0.5:3000")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
 
