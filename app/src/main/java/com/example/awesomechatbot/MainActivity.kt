0960135cf3d5c839a94cdbbd05b351470bd0ced4
rosesua318 <rosesua@duksung.ac.kr>
2021-08-02 20:42:20 +0900

    update

--- app/src/main/java/com/example/awesomechatbot/MainActivity.kt
+++ app/src/main/java/com/example/awesomechatbot/MainActivity.kt
@@ -101,6 +101,7 @@ class MainActivity : AppCompatActivity() {
         setFragment(fragment)
     }
 
+    
     fun setDataAtFragmentHome(fragment: Fragment, data: RecordItem) {
         val bundle = Bundle()
         bundle.putSerializable("item", data)
